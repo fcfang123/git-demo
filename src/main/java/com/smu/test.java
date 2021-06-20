627a811b93f6cf1879063b03f35c6dffea01d6ea
“u <851485863@qq.com>
2021-06-20 21:04:08 +0800

    fc commit

--- src/main/java/com/smu/test.java
+++ src/main/java/com/smu/test.java
@@ -1,7 +1,15 @@
 package com.smu;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("abc");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
     }
 }
